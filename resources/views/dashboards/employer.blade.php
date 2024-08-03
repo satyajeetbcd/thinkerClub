2632408afe6c7c50901230d6a9863586285766f5
Satyajeet Singh <satyajeetbcd@gmail.com>
2024-08-03 16:52:33 +0530

    my commit

--- resources/views/dashboards/employer.blade.php
+++ resources/views/dashboards/employer.blade.php
@@ -33,14 +33,25 @@
                              
                             </div>
                         </div>
+                        @can('manage-job')     
                         <form action="{{ route('dashboard.index') }}" method="GET" class="mb-3">
-        <div class="input-group">
-            <input type="text" name="search" class="form-control" placeholder="Search for pitches..." value="{{ request('search') }}">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="submit">Search</button>
-            </div>
-        </div>
-    </form>
+                            <div class="input-group">
+                                <input type="text" name="search" class="form-control" placeholder="Search for pitches..." value="{{ request('search') }}">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="submit">Search</button>
+                                </div>
+                            </div>
+                        </form>
+                        @else
+                        <form action="{{ route('joblist.index') }}" method="GET" class="mb-3">
+                            <div class="input-group">
+                                <input type="text" name="search" class="form-control" placeholder="Search for pitches..." value="{{ request('search') }}">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="submit">Search</button>
+                                </div>
+                            </div>
+                        </form>
+                        @endcan
                         <div class="card-body">
                         <table class="table  table-borderless table-responsive-sm table-responsive-lg table-responsive-md table-responsive-xl" id="jobs_table">
     <thead>
