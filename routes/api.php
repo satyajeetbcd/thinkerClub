e3f39705de5dae03015aca2470540a7f4bbd722e
Satyajeet Singh <satyajeetbcd@gmail.com>
2024-10-03 12:42:08 +0530

    my commit

--- routes/api.php
+++ routes/api.php
@@ -36,6 +36,8 @@ Route::post('password/reset', [PasswordResetController::class, 'sendResetPasswor
 Route::post('password/update', [PasswordResetController::class, 'reset']);
 Route::get('activate', [AuthAPIController::class, 'verifyAccount']);
 Route::post('/payment', [PaymentController::class, 'store2']);
+Route::any('payment/success', [PaymentController::class, 'success2']);
+Route::any('payment/failure', [PaymentController::class, 'failure2']);
 Route::get('/products', [DashboardController::class, 'productsDashboard']);
 Route::middleware(['auth:api', 'user.activated'])->group(function () {
     Route::post('broadcasting/auth', [BroadcastController::class, 'authenticate']);
